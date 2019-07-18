f5903882ec0ac0fa1b789b25c913413585cf8498
ElizaIra <you@example.com>
2019-07-18 17:32:02 +0800

    showing all employee data inside the dummy api in the web page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 
   class App extends Component {
+    //rendering data inside the console log
     constructor(props) {
       super(props);
       this.state = {employees: []};
@@ -18,8 +19,10 @@ import axios from 'axios';
         }).then(response => {
           console.log('All Employee Details');
           console.log(response.data);
-
-          this.setState({ data: response.data });
+          //rendering data inside the console log 
+          //note: change the employees: to data will show the data in console log only
+          //else it will show in the web page 
+          this.setState({ employees: response.data });
         });
       } catch (err){
         console.log('Error on fetching all employee details');
@@ -30,9 +33,28 @@ import axios from 'axios';
     render(){
      
       return (
-        <div className='App'>
-         <header className='App-header'>
-
+        <div className='container'>
+         <header className='App'>
+            <table>
+              <tbody>
+                <tr>
+                  <th>ID</th>
+                  <th>Employee Name</th>
+                  <th>Age</th>
+                  <th>Salary</th>
+                </tr>
+                {
+                  this.state.employees.map(employees => (
+                    <tr key={employees.id}>
+                      <td>{employees.id}</td>
+                      <td>{employees.employee_name}</td>
+                      <td>{employees.employee_age}</td>
+                      <td>{employees.employee_salary}</td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
          </header>
         </div>
       );
